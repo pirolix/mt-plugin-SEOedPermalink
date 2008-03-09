87a337088efd68248431781cf4a1f3231b2973e8
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2008-03-09 00:28:08 +0000

    0.30 DEVEL
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/SEOedPermalink/trunk@41 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ plugins/SEOedPermalink/SEOedPermalink.pl
@@ -0,0 +1,119 @@
+package MT::Plugin::OMV::SEOedPermalink;
+my $DESCRIPTION = <<'PERLHEREDOC';
+#   SEOedUrl - Handle the optimized URL for some search engines and guide their web crawler.
+#           Programmed by Piroli YUKARINOMIYA
+#           Open MagicVox.net - http://www.magicvox.net/home.php
+#           @see http://www.magicvox.net/
+PERLHEREDOC
+
+use strict;
+use Jcode;
+use MT::Util qw( encode_php encode_url );
+use MT::Template::Context;
+
+use vars qw( $MYNAME $VERSION );
+$MYNAME = 'SEOedUrl';
+$VERSION = '0.30 DEVEL';
+
+### Register a plugin
+use base qw( MT::Plugin );
+my $plugin = new MT::Plugin({
+        name => $MYNAME,
+        version => $VERSION,
+        author_name => 'Piroli YUKARINOMIYA',
+        author_link => "http://www.magicvox.net/?$MYNAME",
+        doc_link => "http://www.magicvox.net/?$MYNAME",
+        description => <<HTMLHEREDOC,
+Handle the optimized URL for some search engines and guide their web crawler.
+Google, Yahoo!, MSN and Baidu are applied.
+HTMLHEREDOC
+});
+MT->add_plugin( $plugin );
+
+sub instance { $plugin; }
+
+
+
+### $MTRedirectSEOedUrl$
+MT::Template::Context->add_tag( RedirectSEOedUrl => \&redirect_seoed_url );
+sub redirect_seoed_url {
+    my ( $ctx, $args, $cond ) = @_;
+
+    # original permalink of entry
+    my $permalink = MT::Template::Context::_hdlr_entry_permalink(@_)
+        or return;
+    $permalink = encode_php( $permalink, 'q' );
+    # SEDed permalink
+    my $seded_permalink = entry_seoed_permalink(@_)
+        or return;
+    $seded_permalink = encode_php( $seded_permalink, 'q' );
+
+    # Generate PHP codes
+    my $php = <<"PHPSOURCECODE";
+/************************************************************************
+$DESCRIPTION
+*/
+function SEOedUrl_getPermalink () { return '$permalink'; }
+function SEOedUrl_getSEOedPermalink () { return '$seded_permalink'; }
+PHPSOURCECODE
+
+    $php .= <<'PHPSOURCECODE';
+function SEOedUrl_isSearchEngineCrawler() {
+	$ua = $_SERVER['HTTP_USER_AGENT'];
+	return preg_match( '/Googlebot\/\d+\.\d+/i', $ua ) // Google
+		|| preg_match( '/Yahoo!/i', $ua ) && preg_match( '/Slurp/i', $ua ) // Yahoo!
+		|| preg_match( '/^msnbot/i', $ua ) // MSN
+		|| preg_match( '/^Baiduspider/i', $ua ) // Baidu
+		|| 0;
+}
+
+function SEOedUrl_movedPermanently( $url ) {
+	header( $_SERVER['SERVER_PROTOCOL']. ' 301 Moved Permanently' );
+	header( 'Status: 301 Moved Permanently' );
+	header( 'Location: '. $url );
+	exit;
+}
+
+function SEOedUrl_switch() {
+	if( !is_array( $parsed_url = parse_url( SEOedUrl_getPermalink())))
+		return;
+	$request_uri = preg_replace( '/\?.*$/', '', $_SERVER['REQUEST_URI'] );
+	if( SEOedUrl_isSearchEngineCrawler()) {
+		if( $request_uri === $parsed_url['path'] )
+			SEOedUrl_movedPermanently( SEOedUrl_getSEOedPermalink());
+	} else {
+		if( $request_uri !== $parsed_url['path'] )
+			SEOedUrl_movedPermanently( SEOedUrl_getPermalink());
+	}
+}
+SEOedUrl_switch();
+PHPSOURCECODE
+
+    "<?php\n${php}?>";
+}
+
+### $MTEntrySEOedPermalink$
+MT::Template::Context->add_tag( EntrySEOedPermalink => \&entry_seoed_permalink );
+sub entry_seoed_permalink {
+    # original permalink
+    my $permalink = MT::Template::Context::_hdlr_entry_permalink(@_)
+        or return;
+    # entry title
+    my $title = MT::Template::Context::_hdlr_entry_title(@_)
+        or return;
+    $title = encode_url( jcode( $title )->utf8 );
+    $title =~ s/%20/_/g;
+
+    # include the utf8 encoded title string in permalink
+    $permalink =~ s!/+$!/$title!;
+    $permalink;
+}
+
+1;
+
+__END__
+========================================================================
+2007/09/19  0.30    全面的に刷新
+2007/05/07  0.21    判定部分に urldecode を追加
+2007/04/24  0.20    大幅に単純化
+2007/04/23  0.10    初版作成
